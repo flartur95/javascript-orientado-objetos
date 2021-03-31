c6ea33640986139dc8230d762282d0fad14f4193
Artur <flartur95@gmail.com>
2021-03-31 12:21:45 -0300

    primeira classe

--- index.js
+++ index.js
@@ -6,11 +6,18 @@ class Cliente{
 }
 
 const cliente1 = new Cliente();
+const cliente2 = new Cliente();
 
 cliente1.nome = "Artur";
 cliente1.cpf = 11123233309;
 cliente1.agencia = 1001;
 cliente1.saldo = 0;
 
+cliente2.nome = "Raquel";
+cliente2.cpf = 88823233309;
+cliente2.agencia = 1001;
+cliente2.saldo = 0;
+
 console.log(cliente1);
+console.log(cliente2);
 
