7cdc25bd6617e9e727bed60ab33fb03e42fa11e9
Artur <flartur95@gmail.com>
2021-03-31 15:16:13 -0300

    null e undefined

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const cliente1 = new Cliente();
 cliente1.nome = "Artur";
 cliente1.cpf = 11123233309;
 
-const cliente2 = new Cliente();
-cliente2.nome = "Raquel";
-cliente2.cpf = 88823233309;
+// const cliente2 = new Cliente();
+// cliente2.nome = "Raquel";
+// cliente2.cpf = 88823233309;
 
 const contaCorrenteArtur = new ContaCorrente();
 contaCorrenteArtur.agencia = 1001;
@@ -16,7 +16,11 @@ contaCorrenteArtur.depositar(500);
 
 const contaCorrenteRaquel = new ContaCorrente();
 contaCorrenteRaquel.agencia = 1002;
-contaCorrenteRaquel.cliente = cliente2;
+// contaCorrenteRaquel.cliente = cliente2;
+contaCorrenteRaquel.cliente = new Cliente();
+contaCorrenteRaquel.cliente.nome = "Raquel";
+contaCorrenteRaquel.cliente.cpf = 88811122209;
+
 
 contaCorrenteArtur.transferir(200, contaCorrenteRaquel);
 
