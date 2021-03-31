34d02eef60f81c37e83f05b612490b81ab20feb0
Artur <flartur95@gmail.com>
2021-03-31 15:50:02 -0300

    commit final

--- index.js
+++ index.js
@@ -5,14 +5,6 @@ const cliente1 = new Cliente("Artur", 11123233309);
 const cliente2 = new Cliente("Raquel", 88823233309);
 
 const contaCorrenteArtur = new ContaCorrente(cliente1, 1001);
-contaCorrenteArtur.depositar(500);
-
 const contaCorrenteRaquel = new ContaCorrente(cliente2, 1002);
 
-contaCorrenteArtur.transferir(200, contaCorrenteRaquel);
-
-console.log(contaCorrenteArtur);
-console.log(contaCorrenteRaquel);
-
-console.log(ContaCorrente.numeroDeContas);
 
