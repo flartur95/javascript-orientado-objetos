98f6dd7ec0d48046922e347660fe17cc79ba009c
Artur <flartur95@gmail.com>
2021-03-31 12:29:56 -0300

    adicionando classe conta corrente

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 class Cliente{
     nome;
     cpf;
+}
+
+class ContaCorrente{
     agencia;
     saldo;
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "Artur";
 cliente1.cpf = 11123233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Raquel";
 cliente2.cpf = 88823233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
+
+const contaCorrenteArtur = new ContaCorrente();
+contaCorrenteArtur.saldo = 0;
+contaCorrenteArtur.agencia = 1001;
 
 console.log(cliente1);
 console.log(cliente2);
