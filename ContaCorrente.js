254e970bb5dce3862555152603164356a9419c9c
Artur <flartur95@gmail.com>
2021-03-31 13:11:49 -0300

    modulos contacorrente

---
+++ ContaCorrente.js
@@ -0,0 +1,18 @@
+class ContaCorrente{
+    agencia;
+    _saldo = 0; // convenção atributo privado
+
+    sacar(valor){
+        if(this._saldo >= valor){
+            this._saldo -= valor; // mesma coisa que this.saldo = this.saldo - valor
+            return valor;
+        }
+    }
+
+    depositar(valor){
+        if(valor <= 0){
+           return;
+        }
+        this._saldo += valor;
+    }
+}
