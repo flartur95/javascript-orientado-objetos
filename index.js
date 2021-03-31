4e1bccbfd23e8ac1e600a633770353b2a819bbf0
Artur <flartur95@gmail.com>
2021-03-31 12:40:07 -0300

    comportamento saque

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ class Cliente{
 class ContaCorrente{
     agencia;
     saldo;
+
+    sacar(valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor; // mesma coisa que this.saldo = this.saldo - valor
+        }
+    }
 }
 
 const cliente1 = new Cliente();
@@ -17,9 +23,13 @@ cliente2.nome = "Raquel";
 cliente2.cpf = 88823233309;
 
 const contaCorrenteArtur = new ContaCorrente();
-contaCorrenteArtur.saldo = 0;
+contaCorrenteArtur.saldo = 100;
 contaCorrenteArtur.agencia = 1001;
+
+contaCorrenteArtur.sacar(50);
+
 
 console.log(cliente1);
 console.log(cliente2);
+console.log(contaCorrenteArtur);
 
