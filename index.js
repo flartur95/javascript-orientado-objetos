7d68fdd8bfc1f692194e70928eaff2822f1c7c78
Artur <flartur95@gmail.com>
2021-03-31 12:10:05 -0300

    primeiros clientes

---
+++ index.js
@@ -0,0 +1,12 @@
+const cliente1Nome = "Artur";
+const cliente1CPF = 11123233309;
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0;
+
+const cliente2Nome = "Raquel";
+const cliente2CPF = 88123233309;
+const cliente2Agencia = 1001;
+const cliente2Saldo = 0;
+
+console.log(cliente1Nome);
+
