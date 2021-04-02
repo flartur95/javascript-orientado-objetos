333eedf9c080c1d2956ffc9dc2bfee022c2148ea
Artur <flartur95@gmail.com>
2021-04-02 18:14:12 -0300

    polimorfismo e autenticar

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ import {Diretor} from "./Funcionario/Diretor.js";
 import {SistemaAutenticacao} from "./SistemaAutenticacao.js";
 
 const diretor = new Diretor("Artur", 10000, 12323432432);
+diretor.cadastrarSenha("123456");
 const gerente = new Gerente("Legolas", 5000, 201210291029121);
+gerente.cadastrarSenha("123");
+const cliente = new Cliente("Artur Cliente", 86711122271, 1234);
 
-const estaLogado = SistemaAutenticacao.login(diretor, "123456");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "1234");
 
-console.log(estaLogado);
+console.log(diretorEstaLogado);
+console.log(gerenteEstaLogado);
+console.log(clienteEstaLogado);
 
 
 
